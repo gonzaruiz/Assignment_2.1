a92837d8df5e7713adbc20040dd88f16fa5cfcda
gonzalo ruiz <38412697+gonzaruiz@users.noreply.github.com>
2018-05-14 10:44:09 -0500

    Add files via upload

---
+++ Assigment_2.1.py
@@ -0,0 +1,12 @@
+
+# coding: utf-8
+
+# In[3]:
+
+
+number = input("enter comma separated numbers: ")
+list = str(number).split(",")
+tuple = (list)
+print('List: ', list)
+print('Tuple: ', tuple)
+
